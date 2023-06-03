29822f466c9b3309c2b1132d2deeb05a25f94cce
Kooreis <kooreis.official@gmail.com>
2023-06-03 14:53:34 -0500

    This update introduces the BinaryTree class, which includes a root node and a method to find the lowest common ancestor of two nodes. The method uses recursion to traverse the tree and find the common ancestor.

--- solution.ts
+++ solution.ts
@@ -1,11 +1,26 @@
-class Node {
-    value: number;
-    left: Node | null;
-    right: Node | null;
+class BinaryTree {
+    root: Node | null;
 
-    constructor(value: number) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
+    constructor() {
+        this.root = null;
+    }
+
+    findLowestCommonAncestor(root: Node | null, value1: number, value2: number): Node | null {
+        if (!root) {
+            return null;
+        }
+
+        if (root.value === value1 || root.value === value2) {
+            return root;
+        }
+
+        const left = this.findLowestCommonAncestor(root.left, value1, value2);
+        const right = this.findLowestCommonAncestor(root.right, value1, value2);
+
+        if (left && right) {
+            return root;
+        }
+
+        return left ? left : right;
     }
 }
